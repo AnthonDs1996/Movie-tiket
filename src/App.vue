c06e4432e472bff4f5d13447130b3f39a1d6737a
anthonPua <anthonds1996@gmail.com>
2024-03-05 21:35:54 +0700

    inisial commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <header>
+     <app-header/>
+    </header>
+    <main>
+      <div class="container">
+        <div class="title">
+          <h1>NOW SHOWING IN CINEMAS</h1>
+        </div>
+        <div class="movies">
+          <app-movie v-for="(data, index) in dataMovie" :key="index" :movie="data" />
+        </div>
+      </div>
+    </main>
+    <footer>
+     <app-footer/>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Movie from "./components/Movie.vue";
+
+export default {
+  data () {
+    return {
+      dataMovie: [
+        {title: "INANG", imageName: "movie1.jpg"},
+        {title: "KALIAN PANTAS MATI", imageName: "movie2.jpg"},
+        {title: "HALOWEEN ENDS", imageName: "movie3.jpg"},
+        {title: "PAMALI", imageName: "movie4.jpg"},
+      ]
+    }
+  },
+
+  components: {
+  "app-header": Header,
+  "app-footer": Footer,
+  "app-movie": Movie,
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+  font-family: "Roboto", sans-serif;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+header {
+  position: sticky;
+  top: 0px;
+  background-color: white;
+}
+
+.container {
+  margin-left: 200px;
+  margin-right: 200px;
+}
+
+
+
+/* Now Showing */
+.movies {
+  display: flex;
+  justify-content: center;
+}
+
+
+
+.title h1 {
+  font-size: 25px;
+}
+
+
+</style>
---
+++ src/components/Movie.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="movie-detail">
+        <img :src="movieImage" alt="Movie">
+        <p>{{ movieTitle }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["movie"],
+    data () {
+        return {
+            movieTitle: this.movie.title,
+            movieImage: require(`../assets/${this.movie.imageName}`)
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    .movie-detail img {
+  border-radius: 20px;
+  width: 230px;
+  height: 350px;
+}
+
+.movie-detail {
+  margin: 7px;
+}
+
+.movie-detail p {
+  margin-top: 15px;
+  font-weight: 1000;
+}
+</style>
